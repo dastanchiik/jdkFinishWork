0561cd2dccb7a2f23153310788fd571cb37f6f48
dastanchick <dosyafree32@gmail.com>
2022-11-19 14:40:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Student> students = new ArrayList<>();
+        System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+        int num = scanner.nextInt();
+        while (true){
+            if (num == 1) {
+                students.add(new Student(scanner.next(), scanner.next(), scanner.nextInt()));
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+                num = scanner.nextInt();
+            }
+            if (num==2){
+                System.out.println("Index:");
+                int a = scanner.nextInt();
+                students.remove(a);
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+                num = scanner.nextInt();
+            }
+            if (num == 3){
+                System.out.println("\n"+students);
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+                num = scanner.nextInt();
+
+            }
+        }
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+
+    private String name;
+    private String lastName;
+    private int age;
+
+    public Student(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}'+"\n";
+    }
+}
