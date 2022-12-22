d3cc4c2c71f0e4fb26556e3ff5a6e0712493fa15
dastanchick <dosyafree32@gmail.com>
2022-12-22 11:02:22 +0600

    I am added

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Student> students = new ArrayList<>();
-        System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+        System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты \n 4 -> удалить всех студентов \n 5 -> to finish");
         int num = scanner.nextInt();
-        while (true){
+        while (num!=5){
             if (num == 1) {
                 students.add(new Student(scanner.next(), scanner.next(), scanner.nextInt()));
-                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты \n 4 -> удалить всех студентов \n 5 -> to finish");
                 num = scanner.nextInt();
             }
-            if (num==2){
+           else if (num==2){
                 System.out.println("Index:");
                 int a = scanner.nextInt();
                 students.remove(a);
-                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты \n 4 -> удалить всех студентов \n 5 -> to finish");
                 num = scanner.nextInt();
             }
-            if (num == 3){
+           else if (num == 3){
                 System.out.println("\n"+students);
-                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты");
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты \n 4 -> удалить всех студентов \n 5 -> to finish");
+                num = scanner.nextInt();
+            } else if (num == 4) {
+                students.clear();
+                System.out.println("1 -> добавить студента" + "\n" + "2 -> удалить студента" + "\n" + "3 -> все студенты \n 4 -> удалить всех студентов \n 5 -> to finish");
                 num = scanner.nextInt();
             }
         }
+        System.out.println("finish \uD83C\uDFC1");
     }
 }
